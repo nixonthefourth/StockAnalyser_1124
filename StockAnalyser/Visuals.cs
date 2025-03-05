5bc8dc187595cbfe56c90e638a41679cc5f21f67
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-03-05 14:06:36 +0000

    Creating visuals of a typewriter in order to make the program's pereption more good-looking

--- StockAnalyser/Visuals.cs
+++ StockAnalyser/Visuals.cs
@@ -2,6 +2,14 @@ namespace StockAnalyser
 {
     public class Visuals
     {
-    
+        // Typewriter Visual
+        public static void DisplayMessage(string message)
+        {
+            for (int i = 0; i < message.Length; i++)
+            {
+                Console.Write(message[i]);
+                System.Threading.Thread.Sleep(30);
+            }
+        }
     }
 }
