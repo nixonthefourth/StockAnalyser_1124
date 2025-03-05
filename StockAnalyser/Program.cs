8602696da68ef1a86f1b3f5a071b8ed65307faab
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-03-05 13:55:18 +0000

    Created file structure in order to ensure good implementation of encapsulation and abstraction.
    Program.cs – Main File, program entry point
    
    Solution.cs – Dumpster that combines all the files and methods from these files in order to properly implement the program.
    
    Sort.cs – Sorting algorithms are stored there (both ascending and descending)
    
    Search.cs – Search algorithms are stored there.
    
    Visuals.cs – Shows the visualiser for terminal, so data has a pleasant output.

--- StockAnalyser/Program.cs
+++ StockAnalyser/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace StockAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
 
-Console.WriteLine("Hello, World!");
---
+++ StockAnalyser/Search.cs
@@ -0,0 +1,7 @@
+namespace StockAnalyser
+{
+    public class Search
+    {
+    
+    }
+}
