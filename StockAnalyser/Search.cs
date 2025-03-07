e5203280194edf1c870d29f3a3ba26b4dcd5bb5e
Nick Khomiakov <72936218+nixonthefourth@users.noreply.github.com>
2025-03-07 11:56:22 +0000

    Fixed the list index issue, deleted redundant parentheses and returns.

--- StockAnalyser/Search.cs
+++ StockAnalyser/Search.cs
@@ -11,39 +11,44 @@ namespace StockAnalyser
         // Recursive Binary Search Implementation
         public int BinarySearch(int[] array, int start, int end, int key)
         {
-            // Base Condition That Stops Loop In The End Of The Array
-            if (start > end)
+            if (array.Contains(key) == true)
             {
-                // Displays That Value Isn't Found
-                Visuals.DisplayMessage("Value isn't found".ToUpper());
+                // Base Condition That Stops Loop In The End Of The Array
+                if (start > end)
+                {
+                    // Displays That Value Isn't Found
+                    Visuals.DisplayMessage("Value isn't found".ToUpper());
+                    return -1;
+                }
+            
+                // Calculate Middle Index
+                int mid = start + (end - start) / 2;
+            
+                // Checks Whether The Middle Index Is Equal To The Target
+                if (array[mid] == key)
+                {
+                    // Return That Value Is Found
+                    Visuals.DisplayMessage($"Item found at {mid}!".ToUpper());
+                }
+            
+                // Otherwise, Carry On With Search
+                // Search Right-Hand Data Using Recursion
+                if (array[mid] < key && mid + 1 <= end)
+                {
+                    return BinarySearch(array, mid + 1, end, key);
+                } else if (array[mid] > key && mid - 1 >= start) {
+                    // Search The Left-Hand Side using Recursion
+                    return BinarySearch(array, start, mid - 1, key);
+                }
+            
+                // Value Not Found Safety Case
                 return -1;
             }
             
-            {
-                
-            }
-            
-            // Calculate Middle Index
-            int mid = start + (end - start) / 2;
-            
-            // Checks Whether The Middle Index Is Equal To The Target
-            if (array[mid] == key)
-            {
-                Visuals.DisplayMessage($"Item found at {array[mid]}!".ToUpper());
-                return mid;
-            }
-            
-            // Otherwise, Carry On With Search
-            // Search Right-Hand Data Using Recursion
-            if (array[mid] < key && mid + 1 <= end)
-            {
-                return BinarySearch(array, mid + 1, end, key);
-            } else if (array[mid] > key && mid - 1 >= start) {
-                // Search The Left-Hand Side using Recursion
-                return BinarySearch(array, start, mid - 1, key);
-            }
-            
             // Value Not Found Safety Case
+            Visuals.DisplayMessage("Value isn't found".ToUpper());
+            
+            // Return
             return -1;
         }
         
